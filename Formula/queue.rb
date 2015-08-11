96a282fbf1f11459350060ed5803a2cca68a864b
Tom Anthony <git@tomanthony.co.uk>
2015-08-11 18:57:45 +0100

    queue: 0.1.1 -> 0.1.2

--- Formula/queue.rb
+++ Formula/queue.rb
@@ -1,19 +1,19 @@
 class Queue < Formula
   #desc "A simple set of scripts that allow you to create a 'command queues'."
   homepage "https://github.com/TomAnthony/queue"
-  url "https://github.com/TomAnthony/queue/archive/0.1.1.tar.gz"
-  sha256 "480dda6609913066d80f3eb7fcbccaa8a3308e2c100929d249c0333a8e5425ae"
+  url "https://github.com/TomAnthony/queue/archive/0.1.2.tar.gz"
+  sha256 "d50e6bd775914a2f381fcd65db1f1baf6a88690374119ed4a9e697d6d29ddd96"
 
   def install
 
-    bin.install "qcreate"
+    bin.install "qset"
     bin.install "qtrigger"
     bin.install "qwait"
 
   end
 
   test do
-    system "#{bin}/qcreate", "test"
+    system "#{bin}/qtrigger", "test"
   end
 end
 
