03a569a8ea64d501d8fb387034aafa5ed4b9a5a6
Tom Anthony <git@tomanthony.co.uk>
2015-08-11 19:05:19 +0100

    queue: 0.1.2 -> 0.1.3

--- Formula/queue.rb
+++ Formula/queue.rb
@@ -1,8 +1,8 @@
 class Queue < Formula
   #desc "A simple set of scripts that allow you to create a 'command queues'."
   homepage "https://github.com/TomAnthony/queue"
-  url "https://github.com/TomAnthony/queue/archive/0.1.2.tar.gz"
-  sha256 "d50e6bd775914a2f381fcd65db1f1baf6a88690374119ed4a9e697d6d29ddd96"
+  url "https://github.com/TomAnthony/queue/archive/0.1.3.tar.gz"
+  sha256 "bfbaf7fefe967cff939f1f1178ea39162fef15222d587cd44f9f03d97b632e01"
 
   def install
 
