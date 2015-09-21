7213c7dc96a204ca5399c155ff29cab88a4a5819
Tom Anthony <git@tomanthony.co.uk>
2015-09-21 13:52:58 +0100

    itermocil: 0.1.6 -> 0.1.7

--- Formula/itermocil.rb
+++ Formula/itermocil.rb
@@ -1,8 +1,8 @@
 class Itermocil < Formula
   # desc "iTermocil allows you setup pre-configured layouts of panes in iTerm2."
   homepage "https://github.com/TomAnthony/itermocil"
-  url "https://github.com/TomAnthony/itermocil/archive/0.1.6.tar.gz"
-  sha256 "c22dec9156cd8934713858f3efe772d9aebcba53782d53456ec6bf7040235dec"
+  url "https://github.com/TomAnthony/itermocil/archive/0.1.7.tar.gz"
+  sha256 "d0aebb0bb815569e984ba8428ba66d2bab2f225ade28dc7dfd119a92e277acd6"
 
   resource "PyYAML" do
     url "https://pypi.python.org/packages/source/P/PyYAML/PyYAML-3.11.tar.gz"
